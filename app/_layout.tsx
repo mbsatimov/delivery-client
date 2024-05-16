4f7a059745bc225eb49095b6c2bccb247341ccd3
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-05-16 12:33:07 +0500

    feat: configure tailwind css

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,7 @@
-import { Stack } from 'expo-router';
-import aa from '../app/index'
+import { Slot } from 'expo-router';
+
+import '../global.css';
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+  return <Slot />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="h-full flex items-center justify-center">
+      <Text className="text-green-500 font-bold text-2xl">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
